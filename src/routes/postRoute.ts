fddaa22900afd178cd084c9f3bf2e71e1dbbad21
mirandaweathers <mirandaweathers@gmail.com>
2022-10-25 13:10:52 -0400

    added post routes

--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -7,6 +7,49 @@ let postRouter = express.Router();
 
 let postArray:Post[] = [];
 
+/**
+ * GET /Posts
+ * most recent first
+ */
+postRouter.get('/', (req, res, next) => {
+    res.status(200).send(postArray.map(post => Post.PrintPost(post)));
+});
 
+/**
+ * POST /Posts
+ */
+postRouter.post('/', (req, res, next) => {
+    // if authorized, create new post with title, content, headerImage(optional)
+    // if unauthorized, 401 Unauthorized
+    // if missing title/content, 406 Unacceptable
+});
+
+/**
+ * GET /Posts/:postId
+ */
+postRouter.get('/:postId', (req, res, next) => {
+
+});
+
+/**
+ * PATCH /Posts/:postId
+ */
+ postRouter.patch('/:postId', (req, res, next) => {
+
+});
+
+/**
+ * DELETE /Posts/:postId
+ */
+ postRouter.delete('/:postId', (req, res, next) => {
+
+});
+
+/**
+ * GET /Posts/User/:userId
+ */
+ postRouter.get('/User/:userId', (req, res, next) => {
+
+});
 
 export { postRouter };
