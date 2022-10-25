1cea14a4bdd94eef9789beb49d8d19e8c3560fcc
mirandaweathers <mirandaweathers@gmail.com>
2022-10-25 12:27:11 -0400

    created Post class

---
+++ src/models/Post.ts
@@ -0,0 +1,9 @@
+export class Post {
+    postId:number = 0;
+    createdDate:Date = new Date();
+    title:string = '';
+    content:string = '';
+    userId:string = '';
+    headerImage:string = '';
+    lastUpdated:Date = new Date();
+}
