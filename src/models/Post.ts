cd18b209e515afdfcf409d3d471725930bb1a134
mirandaweathers <mirandaweathers@gmail.com>
2022-10-26 11:22:34 -0400

    added VerifyPost method for new posts

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,4 +1,5 @@
 export class Post {
+    static id:number = 0;
     postId:number = 0;
     createdDate:Date = new Date();
     title:string = '';
@@ -7,8 +8,24 @@ export class Post {
     headerImage:string = '';
     lastUpdated:Date = new Date();
 
-    constructor(title:string, content:string, headerImage:string) {
+    // constructor(title:string, content:string, headerImage?:string) {
+    //     this.postId = Post.id++;
+    //     this.createdDate = new Date();
+    //     this.title = title;
+    //     this.content = content;
+    //     this.userId = '';
+    //     if(headerImage) this.headerImage = headerImage;
+    //     this.lastUpdated = new Date();
+    // }
 
+    static VerifyPost(newPost:any) {
+        if(newPost.title != '' && newPost.content != '') {
+            if(newPost.headerImage != '') {
+                return newPost.title && newPost.content && newPost.headerImage;
+            } else {
+                return newPost.title && newPost.content;
+            }
+        }
     }
 
     static PrintPost(post:Post){
