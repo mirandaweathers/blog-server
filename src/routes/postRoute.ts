aa2cdade5b90d44b35ca03280e16af99cf06f9bd
mirandaweathers <mirandaweathers@gmail.com>
2022-10-26 11:22:47 -0400

    initial post commit

--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -1,11 +1,23 @@
 import { json } from 'body-parser';
 import express from 'express';
 import { Post } from '../models/Post';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { userRouter } from './userRoute';
+import dotenv from 'dotenv';
+dotenv.config();
+const key = process.env.JWTKEY!.toString();
 
 let postRouter = express.Router();
 
 let postArray:Post[] = [];
+
+// function parseJWT(token:string|JwtPayload) {
+//     var baseUrl = token.split(',')[1];
+//     var base = decodeURIComponent(atob(baseUrl).split('').map((c) =>{
+//         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+//     }).join(''));
+//     return base;
+// }
 
 /**
  * GET /Posts
@@ -19,9 +31,56 @@ postRouter.get('/', (req, res, next) => {
  * POST /Posts
  */
 postRouter.post('/', (req, res, next) => {
-    // if authorized, create new post with title, content, headerImage(optional)
-    // if unauthorized, 401 Unauthorized
-    // if missing title/content, 406 Unacceptable
+    if(Post.VerifyPost(req.body)) {
+        let post = new Post();
+        post = {
+            postId: Post.id++,
+            createdDate: new Date(),
+            title: req.body.title,
+            content: req.body.content,
+            userId: 'idk yet',
+            headerImage: req.body.headerImage ?? '',
+            lastUpdated: new Date()
+        }
+        res.status(201).send({message: 'post created'})
+    } else {
+        res.status(400).send({message: 'error'})
+    }
+
+    // // check for authorization header
+    // if(req.headers['authorization']) {
+    //     try {
+    //         let authToken = jwt.verify(req.headers['authorization'].replace('Bearer ',''), key);
+        
+    //         // validate token
+    //         if(authToken) {
+    //             // get userId from authToken
+    //             let userId = parseJWT(authToken);
+    //             console.log(userId);
+
+    //             // create new post with title, content, and optional headerImage
+    //             if(Post.VerifyPost(req.body)) {
+    //                 let post = new Post();
+    //                 post = {
+    //                     postId: Post.id++,
+    //                     createdDate: new Date(),
+    //                     title: req.body.title,
+    //                     content: req.body.content,
+    //                     userId: userId,
+    //                     headerImage: req.body.headerImage ?? '',
+    //                     lastUpdated: new Date()
+    //                 }
+    //             }
+    //             // if missing title or content, 406 Unacceptable
+    //         } else {
+    //             res.status(401).send({message:'Error - Unauthorized - Invalid Token'});
+    //         }
+    //     } catch(e) {
+    //     res.status(401).send({message:'Error - Unauthorized - Invalid Token'});
+    //     } 
+    // } else {
+    //     res.status(401).send({message:'Error - Unauthorized - Invalid Token'});
+    // }
 });
 
 /**
