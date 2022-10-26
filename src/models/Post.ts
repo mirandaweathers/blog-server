1789f8072b545b21c59667c059446aec536eceee
mirandaweathers <mirandaweathers@gmail.com>
2022-10-26 12:49:09 -0400

    updated verify and print methods

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -20,15 +20,19 @@ export class Post {
 
     static VerifyPost(newPost:any) {
         if(newPost.title != '' && newPost.content != '') {
-            if(newPost.headerImage != '') {
-                return newPost.title && newPost.content && newPost.headerImage;
-            } else {
-                return newPost.title && newPost.content;
-            }
+            return newPost.title && newPost.content;
         }
     }
 
     static PrintPost(post:Post){
-
+        return {
+            postId: post.postId,
+            createdDate: post.createdDate,
+            title: post.title,
+            content: post.content,
+            userId: post.userId,
+            headerImage: post.headerImage,
+            lastUpdated: post.lastUpdated
+        }
     }
 }
