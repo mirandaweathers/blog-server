6568d0b59e2c616a048be6140681e08ea9d770ff
mirandaweathers <mirandaweathers@gmail.com>
2022-10-26 15:35:25 -0400

    updated verify and print methods

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -1,15 +1,23 @@
 export class Comment {
+    static id:number = 0;
     commentId:number = 0;
     comment:string = '';
     userId:string = '';
     postId:number = 0;
     commentDate:Date = new Date();
 
-    constructor(comment:string) {
-
+    static VerifyComment(newComment:Comment) {
+        if(newComment.comment != '')
+            return newComment.comment;
     }
 
     static PrintComment(comment:Comment) {
-
+        return {
+            commentId: comment.commentId,
+            comment: comment.comment,
+            userId: comment.userId,
+            postId: comment.postId,
+            commentDate: comment.commentDate
+        }
     }
 }
